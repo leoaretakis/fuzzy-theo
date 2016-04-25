f8d3ea198cb54286e3393d877a8eedbe6cbf5678
Leonardo Aretakis <leoaretakis@gmail.com>
2016-04-25 13:18:47 +0100

    refactor core

--- test/operations/test-is-core.js
+++ test/operations/test-is-core.js
@@ -8,9 +8,7 @@ import isCore from './../../src/operations/is-core';
 FuzzySet.prototype.isCore = function core(...x) { return isCore(this, ...x); };
 
 describe('Core', () => {
-  const fs = new FuzzySet({
-    mf: { func: (x) => roundTo(Math.cos(x), 5) },
-  });
+  const fs = new FuzzySet({ func: (x) => roundTo(Math.cos(x), 5) });
 
   it('is the set of members with grade equal 1', () => {
     expect(isCore(fs, 0)).to.be.true;
