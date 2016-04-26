3a98b06a01b0fcd5fb966d46cc37f9d39efffe4e
Leonardo Aretakis <leoaretakis@gmail.com>
2016-04-27 00:56:11 +0100

    refactor intersection union

--- src/operations/intersection_union.js
+++ src/operations/intersection_union.js
@@ -2,6 +2,10 @@
 import { mergeFuzzySetProps } from './helpers';
 import FuzzySet from './../fuzzy-set';
 
-export default function union(...fsList) {
+export function intersection(...fsList) {
+  return new FuzzySet(mergeFuzzySetProps(Math.min, false, ...fsList));
+}
+
+export function union(...fsList) {
   return new FuzzySet(mergeFuzzySetProps(Math.max, false, ...fsList));
 }
--- test/operations/test-intersection.js
+++ test/operations/test-intersection.js
@@ -2,18 +2,31 @@
 
 import { expect } from 'chai';
 import { FuzzySet } from './../..';
-import intersection from './../../src/operations/intersection';
+import { intersection, union } from './../../src/operations/intersection_union';
 
-describe('Intersection operation', () => {
+describe('Basic operations', () => {
   const fs = new FuzzySet({ func: (x) => Math.round(Math.cos(x)) });
   const fs2 = new FuzzySet({ func: () => 1.0 });
 
-  it('is a set with the min grade between 2 fuzzy sets', () => {
-    const intersectionFS = intersection(fs, fs2);
+  describe('Intersection', () => {
+    it('is a set with the min grade between 2 fuzzy sets', () => {
+      const intersectionFS = intersection(fs, fs2);
 
-    expect(intersectionFS.membershipGrade(-Math.PI / 2.0)).to.equal(0);
-    expect(intersectionFS.membershipGrade(0)).to.equal(1);
-    expect(intersectionFS.membershipGrade(Math.PI / 2.0)).to.equal(0);
-    expect(intersectionFS.membershipGrade(Math.PI)).to.equal(0);
+      expect(intersectionFS.membershipGrade(-Math.PI / 2.0)).to.equal(0);
+      expect(intersectionFS.membershipGrade(0)).to.equal(1);
+      expect(intersectionFS.membershipGrade(Math.PI / 2.0)).to.equal(0);
+      expect(intersectionFS.membershipGrade(Math.PI)).to.equal(0);
+    });
+  });
+
+  describe('Union', () => {
+    it('is a set with the max grade between 2 fuzzy sets', () => {
+      const unionFs = union(fs, fs2);
+
+      expect(unionFs.membershipGrade(-Math.PI / 2.0)).to.equal(1.0);
+      expect(unionFs.membershipGrade(0)).to.equal(1.0);
+      expect(unionFs.membershipGrade(Math.PI / 2.0)).to.equal(1.0);
+      expect(unionFs.membershipGrade(Math.PI)).to.equal(1.0);
+    });
   });
 });
