d924212e419514884db7b7b10ce57b4adbfb48d8
Leonardo Aretakis <leoaretakis@gmail.com>
2016-04-26 18:25:58 +0100

    Coverage tests (#3)
    * first cov tests
    
    * delete cov directory before test
    
    * add del (node 4)
    
    * add to gulp default
    
    * fix coverage
    
    * better display test errors

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,87 @@
 'use strict';
 
 import 'babel-polyfill';
-
 import gulp from 'gulp';
 import mocha from 'gulp-mocha';
 import eslint from 'gulp-eslint';
-// import babel from 'gulp-babel';
+import coverageEnforcer from 'gulp-istanbul-enforcer';
+import istanbul from 'gulp-istanbul';
+import { Instrumenter } from 'isparta';
+import del from 'del';
+import path from 'path';
+
+
+const excludedFiles = ['!node_modules/**', '!coverage/**', '!html-report/**'];
+
+const dirs = {
+  coverageRoot: path.join(__dirname, 'coverage/'),
+  sourceRoot: path.join(__dirname, 'src'),
+  testRoot: path.join(__dirname, 'test'),
+};
+
+const files = {
+  tests: path.join(dirs.testRoot, '/**/*.js'),
+  source: path.join(dirs.sourceRoot, '/**/*.js'),
+  allJs: path.join(__dirname, '/**/*.js'),
+  entryPoint: 'index.js',
+};
+
+gulp.task('clean-coverage', (cb) => del([dirs.coverageRoot, 'html-report/'], cb));
+
+gulp.task('test-coverage', ['clean-coverage'], (cb) => {
+  const coverageDir = dirs.coverageRoot;
+
+  gulp.src([files.source, files.entryPoint, ...excludedFiles])
+    .pipe(istanbul({ instrumenter: Instrumenter, includeUntested: true }))
+    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', () => {
+      gulp.src([files.tests, ...excludedFiles], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', (err) => {
+          // TODO: in the future remove this and put gulp-plumber
+          // eslint-disable-next-line no-console
+          console.log('error', err);
+          process.exit(1);
+        })
+        .pipe(istanbul.writeReports({
+          dir: coverageDir,
+          reportOpts: { dir: coverageDir },
+          reporters: ['text', 'text-summary', 'json', 'html', 'lcov'],
+        }))
+        .pipe(coverageEnforcer({
+          thresholds: {
+            statements: 80,
+            branches: 50,
+            lines: 80,
+            functions: 50,
+          },
+          coverageDirectory: coverageDir,
+          rootDirectory: '',
+        }))
+        .on('end', cb);
+    });
+});
 
 gulp.task('lint', () =>
-  gulp.src(['**/*.js', '!node_modules/**'])
+  gulp.src([files.allJs, ...excludedFiles])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 );
 
 gulp.task('test', () =>
-  gulp.src(['test/**/*.js', '!node_modules/**'])
+  gulp.src([files.tests, ...excludedFiles])
     .pipe(mocha())
 );
 
 gulp.task('watch', () =>
-  gulp.watch(['**/*.js', '!node_modules/**'], ['lint'])
+  gulp.watch([files.allJs, ...excludedFiles], ['lint'])
 );
 
 gulp.task('watch-test', () =>
-  gulp.watch(['**/*.js', '!node_modules/**'], ['test'])
+  gulp.watch([files.allJs, ...excludedFiles], ['test'])
 );
 
-gulp.task('default', ['test', 'lint'], () => {
+gulp.task('default', ['lint', 'test-coverage'], () => {
   // console.log('gulp launched ok');
 });
