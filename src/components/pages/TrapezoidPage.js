6fccdf4b554b318b87564dc7a9f8337811e8c83d
Leonardo Aretakis <leoaretakis@gmail.com>
2016-05-02 01:41:59 +0100

    triangle and trapezoid

--- src/components/pages/TrapezoidPage.js
+++ src/components/pages/TrapezoidPage.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { LineChart } from 'react-d3-basic';
+import { TrapezoidalFS } from 'fuzzy-theo';
 
-const TrapezoidPage = () => (
-  <section>
-  </section>
-);
+const TrapezoidPage = () => {
+  const fs = new TrapezoidalFS(20, 40, 60, 80);
+
+  const chartData = [];
+  for (let i = 0; i <= 100; i = i + 5) {
+    chartData.push({
+      x: i,
+      y: fs.membershipGrade(i),
+    });
+  }
+
+  return (
+    <section>
+      <LineChart
+        showXGrid= {false}
+        showYGrid= {false}
+        margins={{left: 40, right: 40, top: 40, bottom: 40}}
+        data={chartData}
+        width={600}
+        height={280}
+        chartSeries={[{ field: 'y', color: '#ff7f0e' }]}
+        x={(d) => d.x}
+      />
+    </section>
+  );
+};
 
 export default TrapezoidPage;
--- src/components/pages/TrianglePage.js
+++ src/components/pages/TrianglePage.js
@@ -1,12 +1,31 @@
 import React from 'react';
-//import FuzzySet from 'fuzzy-theo';
+import { LineChart } from 'react-d3-basic';
+import { TriangularFS } from 'fuzzy-theo';
 
 const TrianglePage = () => {
 
-  console.log('TrianglePage', FuzzySet);
+  const fs = new TriangularFS(20,40, 60);
+
+  const chartData = [];
+  for (let i = 0; i < 81; i = i + 5) {
+    chartData.push({
+      x: i,
+      y: fs.membershipGrade(i),
+    });
+  }
 
   return (
     <section>
+      <LineChart
+        showXGrid= {false}
+        showYGrid= {false}
+        margins={{left: 40, right: 40, top: 40, bottom: 40}}
+        data={chartData}
+        width={600}
+        height={280}
+        chartSeries={[{ field: 'y', color: '#ff7f0e' }]}
+        x={(d) => d.x}
+      />
     </section>
   );
 };
